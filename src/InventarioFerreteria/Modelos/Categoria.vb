a2bbfcf451a6582f7bba30a6905137c8f564b550
Heyling Guerrero <heyling.guerrero709@std.uni.edu.ni>
2026-09-25 14:26:16 -0600

    feat: modelos Producto y Categoria

---
+++ src/InventarioFerreteria/Modelos/Categoria.vb
@@ -0,0 +1,7 @@
+﻿''' <summary>Representa una fila de la tabla categorias.</summary>
+Public Class Categoria
+
+    Public Property IdCategoria As Integer
+    Public Property Nombre As String = ""
+
+End Class
