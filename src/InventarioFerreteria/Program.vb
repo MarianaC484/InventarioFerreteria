fe9c8aa382fbe0fbc72927462b58dab787690cf3
Mariana Calderon <angelica.moreno324@std.uni.edu.ni>
2026-09-24 18:37:21 -0600

    chore: proyecto WinForms .NET 10 con MySqlConnector

--- src/InventarioFerreteria/Program.vb
+++ src/InventarioFerreteria/Program.vb
@@ -1,11 +1,14 @@
-﻿Friend Module Program
+﻿Imports System.Windows.Forms
 
-    <STAThread()>
-    Friend Sub Main(args As String())
+' Punto de entrada de la aplicación (MyType = WindowsFormsWithCustomSubMain)
+Module Program
+
+    <STAThread>
+    Sub Main()
         Application.SetHighDpiMode(HighDpiMode.SystemAware)
         Application.EnableVisualStyles()
         Application.SetCompatibleTextRenderingDefault(False)
-        Application.Run(New Form1)
+        Application.Run(New FrmProductos())
     End Sub
 
 End Module
